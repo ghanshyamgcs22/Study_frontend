715410609adf5e3291b8522e6d6328cd257f25f9
Ghanshyam Gupta <ghanshyamg.cs.22@nitj.ac.in>
2024-11-05 23:48:32 +0530

    Login

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,7 +3,7 @@ function Hero(){
 <div>
 
 <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
+  <div className="hero-content flex-col lg:flex-row">
     <div className="text-center lg:text-left">
       <h1 className="text-5xl font-bold">Login now!</h1>
       <p className="py-6">
@@ -24,9 +24,16 @@ function Hero(){
             <span className="label-text">Password</span>
           </label>
           <input type="password" placeholder="password" className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
+          <div className="flex justify-between">
+  <label className="label">
+    <a href="#" className="label-text-alt link link-hover text-right">Forgot password?</a>
+  </label>
+  <label className="label">
+    <a href="#" className="label-text-alt link link-hover ">Don't have an account? <a className="font-bold underline"></a>Signup</a>
+  </label>
+</div>
+
+          
         </div>
         <div className="form-control mt-6">
           <button className="btn btn-primary">Login</button>
